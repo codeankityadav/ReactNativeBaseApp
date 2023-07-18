31f00857f5deba4f4ea72e481cefb3c1911c9be9
yadavian <ankit.rama.yadav@gmail.com>
2023-07-18 08:13:09 -0700

    font, tailwind

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -2,12 +2,14 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { AntDesign } from '../utils/constant/Vector'
 import Lottie from 'lottie-react-native'
+import { FONT_FAMILY } from '../utils'
 
 const HomeScreen = () => {
   return (
     <View>
       <Lottie source={require('../asset/lottie/loading.json')} duration={5000} autoPlay loop />
       <AntDesign name="rocket1" size={30} color="#900" />
+      <Text className="text-amber-400" style={{fontFamily : FONT_FAMILY.POPPINS.EXTRA_BOLD_ITALIC}}>Skip</Text>
     </View>
   )
 }
