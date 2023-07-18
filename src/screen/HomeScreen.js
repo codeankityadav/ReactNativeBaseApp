c033fe6a06db88308544c59aa8f89c08c8d50bfd
yadavian <ankit.rama.yadav@gmail.com>
2023-07-18 07:55:04 -0700

    lottie

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { AntDesign } from '../utils/constant/Vector'
+import Lottie from 'lottie-react-native'
 
 const HomeScreen = () => {
   return (
     <View>
+      <Lottie source={require('../asset/lottie/loading.json')} duration={5000} autoPlay loop />
       <AntDesign name="rocket1" size={30} color="#900" />
     </View>
   )
